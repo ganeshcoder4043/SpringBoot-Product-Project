acf491f1675122c1c102adc28e91dd893983a37b
ganeshcoder4043 <ganesha4043@gmail.com>
2026-04-14 15:17:45 +0530

    Complete Logging with notes

--- src/main/java/com/springbootproject/product/ProductApplication.java
+++ src/main/java/com/springbootproject/product/ProductApplication.java
@@ -4,6 +4,9 @@ import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -23,12 +26,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 		)
 )
 @SpringBootApplication
+@Slf4j
 public class ProductApplication {
+
+	// Step 1: Logger instance banao
+//	private static final Logger log = LoggerFactory.getLogger(ProductApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductApplication.class, args);
-		System.out.println("YOURS PROJECTS IS RUNNING.........");
+		//System.out.println("YOURS PROJECTS IS RUNNING.........");
 
+		/* logging*/
+//		log.trace("its trace message");
+//		log.debug("its debug message");
+		log.info("YOURS PROJECTS IS RUNNING.........");
+//		log.warn("its warn message");
+//		log.error("its error message");
+
+		/* Some Important Points*/
+		/*
+		* When u use log annotation -> @Slf4j than don't create logger instance.
+		* When u use logger instance -> @Slf4j annotation don't use.
+		* Both together don't use.
+		* */
 	}
 
 }
